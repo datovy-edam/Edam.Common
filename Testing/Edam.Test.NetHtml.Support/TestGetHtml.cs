d6eeb9e9ef0aaa19fe0fbe8b5be13782bd5d893c
Eduardo Sobrino <datovy-edam@gmail.com>
2025-04-19 15:08:11 -0400

    added HTML support...

---
+++ Testing/Edam.Test.NetHtml.Support/TestGetHtml.cs
@@ -0,0 +1,18 @@
+﻿using Edam.Diagnostics;
+using Edam.Net.Web;
+
+namespace Edam.Test.NetHtml.Support;
+
+[TestClass]
+public sealed class TestGetHtml
+{
+
+   [TestMethod]
+   public void TestMethod1()
+   {
+      string url = "https://projecttracking.technology.ca.gov/PAL";
+      string id = "projectlist";
+      ResultsLog<string> results = HtmlClient.GetHtml(url, id);
+   }
+
+}
