9dd7ab1a21f9fe3663842792237e63b01459a1da
Eduardo Sobrino <datovy-edam@gmail.com>
2025-04-23 17:05:42 -0400

    sync all

--- Testing/Edam.Test.NetHtml.Support/TestGetHtml.cs
+++ Testing/Edam.Test.NetHtml.Support/TestGetHtml.cs
@@ -8,11 +8,30 @@ public sealed class TestGetHtml
 {
 
    [TestMethod]
-   public void TestMethod1()
+   public void TestHtmlScraper()
    {
       string url = "https://projecttracking.technology.ca.gov/PAL";
       string id = "projectlist";
       ResultsLog<string> results = HtmlClient.GetHtml(url, id);
    }
 
+   [TestMethod]
+   public void TestHtmlTableScraper()
+   {
+      Dictionary<string, string> keyValues = new Dictionary<string, string>();
+      keyValues.Add("Title", "Agency Name");
+
+      string url = "https://projecttracking.technology.ca.gov/PAL";
+      ResultsLog<HtmlTableInfo> results = HtmlClient.GetHtmlTable(url);
+      results.Instance.HeaderMap = keyValues;
+      string jsonText = results.Instance.ToJsonDocument();
+   }
+
+   [TestMethod]
+   public void TestTiming()
+   {
+      LogTimingInfo elap = new LogTimingInfo();
+      Task.Delay(1000).Wait();
+      string mess = elap.ToString();
+   }
 }
